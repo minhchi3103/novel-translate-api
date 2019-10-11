249cafe1ffc79aac1b12bbf46c81ee2d7382699d
shinryak <minhchi3103@gmail.com>
2019-10-11 18:04:49 +0700

    change app config

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
-var indexRouter = require('@route/index');
-var usersRouter = require('@route/user');
-var userRegisterRouter = require('@route/user/register')
+var allRouter = require('@r/config.js');
 
+// load private/public key and store in process.env
+process.env.PRIVATE_KEY = fs.readFileSync('jwtRS256.key');
+process.env.PUBLIC_KEY = fs.readFileSync('jwtRS256.key.pub');
+
+// connect mongoDB
 mongoose.connect(process.env.MONGO_DB, {
   useNewUrlParser: true,
   useCreateIndex: true
@@ -18,24 +21,33 @@ mongoose.connect(process.env.MONGO_DB, {
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
+// plugin setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: false
+  extended: true
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/register', userRegisterRouter);
+// use global middleware
+app.use(function (req, res, next) {
+  if (!req.mw) req.mw = {};
+  next();
+})
+app.use(require('@mw/config')['TokenParsingMiddleware'])
+
+// routes setup
+app.use('/', allRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use('/', function (req, res, next) {
+  let err = new Error('not found.');
+  err.status = 404
+  next(err);
 });
 
 // error handler
@@ -43,10 +55,11 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // render the error response
+  res.status(err.status || 500).json({
+    "errorCode": res.statusCode,
+    "errorMessage": err.message
+  })
 });
 
 module.exports = app;
